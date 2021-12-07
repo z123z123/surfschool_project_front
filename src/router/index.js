4b33e2ca5a4d8a2a4d0874cd5b845cd022ea6d44
Tristan Veskilt <tristanveskilt@gmail.com>
2021-12-07 09:09:06 +0200

    Front first commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import BookingSingle from "@/views/BookingSingle";
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+  },
+  {
+    path: "/booking",
+    name: "BookingSingle",
+    component: BookingSingle
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/BookingSingle.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-col cols="12" md="6">
+          <v-date-picker
+              v-model="picker"
+              show-adjacent-months
+          ></v-date-picker>
+        </v-col>
+        <v-col cols="12" md="6">
+          Time picker
+        </v-col>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-row>
+          <v-col cols="12" md="5">
+            <v-form>
+              <v-row>
+                <v-select
+                    :items="items"
+                    label="Surf style"
+                    required
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-select
+                    :items="levels"
+                    label="Skill level"
+                    required
+                ></v-select>
+              </v-row>
+              <v-row>
+                <v-text-field
+                    label="First name"
+                    required
+                ></v-text-field>
+              </v-row>
+              <v-row>
+                <v-text-field
+                    label="Last name"
+                    required
+                ></v-text-field>
+              </v-row>
+              <v-row>
+                <v-text-field
+                    label="E-mail"
+                    required
+                ></v-text-field>
+              </v-row>
+            </v-form>
+          </v-col>
+          <v-col cols="12" md="5">
+            <v-row>
+              <v-text-field
+                  label="Height"
+                  required
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field
+                  label="Weight"
+                  required
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field
+                  label="Gender"
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-checkbox
+                  v-model="checkbox"
+                  :label="`Require wetsuit`"
+              ></v-checkbox>
+            </v-row>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-btn>Book</v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "BookingSingle",
+  data: () => ({
+    items: ["Surf", "Windsurf", "Kitesurf"],
+    levels: ["Beginner", "Intermediate", "Advanced"]
+  }),
+
+}
+</script>
+
+<style scoped>
+
+</style>

4a9841dd5bdeb85bd576e14d0e26d627c112b452
z123z123 <zenyanov@gmail.com>
2021-12-07 15:24:32 +0200

    front Evg

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,38 +1,25 @@
 <template>
   <v-container px-0 py-0 fluid>
-    <v-img max-height="786"
-           src="https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2020/5/1/mqgjwevxveuc6fcqvdfc/carissa-moore-waves"></v-img>
-    <v-row class="ma-10">
-      <v-col align-self="center" cols="12" md="6">
-        <h1>Traditional surf</h1>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-      </v-col>
-      <v-col align="center" cols="12" md="6">
-        <v-img height="350" max-width="300" src="https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2016/05/22/755/500/8f03b7f699a9e7fc527676a80c5dce92.jpg?mod=v1_e77e63ed868019f4c5a56e2a7c532710"></v-img>
-      </v-col>
-    </v-row>
-    <v-row class="ma-10">
-      <v-col align="center" cols="12" md="6">
-        <v-img height="350" max-width="300" src="https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2016/05/22/755/500/8f03b7f699a9e7fc527676a80c5dce92.jpg?mod=v1_e77e63ed868019f4c5a56e2a7c532710"></v-img>
-      </v-col>
-      <v-col align-self="center" cols="12" md="6">
-        <h1>Traditional surf</h1>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-      </v-col>
-    </v-row>
-    <v-row class="ma-10">
-      <v-col align-self="center" cols="12" md="6">
-        <h1>Traditional surf</h1>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-      </v-col>
-      <v-col align="center" cols="12" md="6">
-        <v-img  height="350" max-width="300" src="https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2016/05/22/755/500/8f03b7f699a9e7fc527676a80c5dce92.jpg?mod=v1_e77e63ed868019f4c5a56e2a7c532710"></v-img>
-      </v-col>
-    </v-row>
-    <video src="production ID_4782485.mp4" muted loop autoplay ></video>
+    <section class="showcase">
+
+      <video src="Surf_video.mp4" muted autoplay loop></video>
+      <div class="overlay">
+        <div class="text">
+          <h2 class="h">Unforgettable Surf Experience</h2>
+          <h2>Fuerteventura Awaits</h2>
+          <p>One of the most amazing places to learn surfing and up your skill</p>
+        </div>
+      </div>
+
+    </section>
+
+    <button @booking="booking()">Book Your Experience Now</button>
+
+    <ul class="social-icon">
+      <li><img src="facebook.png" alt=""></li>
+      <li><img src="instagram.png" alt=""></li>
+
+    </ul>
   </v-container>
 </template>
 
@@ -43,5 +30,85 @@ export default {
 </script>
 
 <style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.showcase {
+  position: absolute;
+  right: 0;
+  width: 100%;
+  min-height: 100vh;
+  padding: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: antiquewhite;
+  color: blue;
+  z-index: 2;
+}
+
+.showcase header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 40px 100px;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #c48a0f;
+  font-family: "Bauhaus 93";
+  font-size: x-large;
+}
+
+.showcase video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: 0.8;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+}
+
+.text {
+  /*position: relative;*/
+  /*z-index: 10;*/
+  z-index: 1000;
+  display: block;
+  align-items: center;
+  justify-content: space-between;
+}
+
+botton {
+  font-family: "Bauhaus 93";
+  color: rgba(250, 222, 118, 0.99);
+}
+
+section {
+  font-family: "Bauhaus 93";
+  font-size: large;
+}
+
+
+h2 {
+  color: #c48a0f;
+  font-family: "Bauhaus 93";
+  font-size: large;
+}
+
 
 </style>

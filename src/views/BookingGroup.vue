431c1f32534e4f58076889228d18c96a56c536d1
z123z123 <zenyanov@gmail.com>
2021-12-10 16:49:09 +0200

    front: booking confirmation page countdown remove

--- src/views/BookingGroup.vue
+++ src/views/BookingGroup.vue
@@ -1,127 +1,214 @@
 <template>
-  <v-container fluid>
-    <h1>Booking Group (max 6 people)</h1>
-
-    <v-form v-model="valid">
-      <v-row>
-
-        <v-col cols="12" md="4">
-
-        </v-col>
-        <v-col cols="12" md="4">
-          <v-text-field
-              v-model="style"
-              label="Surf style"
-              required
-          ></v-text-field>
-          <v-text-field
-              v-model="skill"
-              label="Skill level"
-              required
-          ></v-text-field>
-          <v-text-field
-              v-model="firstname"
-              label="First name"
-              required
-          ></v-text-field>
-          <v-text-field
-              v-model="lastname"
-              label="Last name"
-              required
-          ></v-text-field>
-          <v-text-field
-              v-model="email"
-              label="E-mail"
-              required
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12" md="4">
-          <v-text-field
-              v-model="height"
-              label="Height"
-              required
-          ></v-text-field>
-          <v-text-field
-              v-model="weight"
-              label="Weight"
-              required
-          ></v-text-field>
-          <v-text-field
-              v-model="gender"
-              label="gender"
-          ></v-text-field>
-          <v-text-field
-              v-model="wetsuit_required"
-              label="Wetsuit required"
-              required
-          ></v-text-field>
-        </v-col>
-
-      </v-row>
-    </v-form>
-
-
+  <v-container fluid class="teal lighten-5">
+    <v-btn v-on:click="back()" class="teal darken-3 white--text back-btn">Back</v-btn>
     <v-row>
-      <v-col cols="12" sm="6">
-        <v-date-picker
-            v-model="dates"
-            multiple
-        ></v-date-picker>
-      </v-col>
-      <v-col
-          cols="12"
-          sm="6"
-      >
-        <v-menu
-            ref="menu"
-            v-model="menu"
-            :close-on-content-click="false"
-            :return-value.sync="dates"
-            transition="scale-transition"
-            offset-y
-            min-width="auto"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-combobox
-                v-model="dates"
-                multiple
-                chips
-                small-chips
-                label="Multiple picker in menu"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-            ></v-combobox>
-          </template>
-          <v-date-picker
-              v-model="dates"
-              multiple
-              no-title
-              scrollable
+      <v-col cols="12" md="4">
+        <img src="booking-logo.png" alt="background-img" class="background-img">
+        <div class="data-input-top">
+          <v-menu
+              ref="menu"
+              v-model="menu"
+              :close-on-content-click="false"
+              :return-value.sync="date"
+              transition="scale-transition"
+              offset-y
+              min-width="auto"
           >
-            <v-spacer></v-spacer>
-            <v-btn
-                text
-                color="primary"
-                @click="menu = false"
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                  v-model="date"
+                  label="Select date"
+                  v-bind="attrs"
+                  v-on="on"
+                  outlined
+                  background-color="white"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+                v-model="date"
+                no-title
+                scrollable
+                width="340"
             >
-              Cancel
+              <div class="time-input">
+                <v-select
+                    :items="times"
+                    label="See available times"
+                    v-model="time"
+                    :rules="[(v) => !!v || 'Field is required']"
+                    outlined
+                    width="350"
+                    class=""
+                    required> Select time
+                </v-select>
+                <div class="d-flex justify-space-between">
+                  <v-btn
+                      text
+                      color="indigo darken-4"
+                      @click="menu = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                      text
+                      color="indigo darken-4"
+                      @click="$refs.menu.save(date)"
+                  >
+                    OK
+                  </v-btn>
+                </div>
+              </div>
+            </v-date-picker>
+          </v-menu>
+        </div>
+      </v-col>
+      <v-col cols="12" md="5">
+        <div class="data-input-view">
+          <v-select
+              :items="styles"
+              item-text="style"
+              label="Surf style"
+              v-model="client.style"
+              :rules="[(v) => !!v || 'Field is required']"
+              return-object
+              background-color="white"
+              required
+              outlined
+          ></v-select>
+          <v-select
+              :items="levels"
+              label="Surf level"
+              v-model="client.level"
+              :rules="[(v) => !!v || 'Field is required']"
+              outlined
+              required
+              background-color="white"
+          ></v-select>
+          <v-text-field
+              v-model="client.firstName"
+              :rules="nameRules"
+              label="First name"
+              outlined
+              required
+              background-color="white"
+          ></v-text-field>
+          <v-text-field
+              v-model="client.lastName"
+              :rules="nameRules"
+              label="Last name"
+              outlined
+              required
+              background-color="white"
+          ></v-text-field>
+          <v-text-field
+              v-model="client.email"
+              :rules="emailRules"
+              label="E-mail"
+              outlined
+              required
+              background-color="white"
+          ></v-text-field>
+          <v-text-field
+              v-model="client.height"
+              :rules="heightRules"
+              label="Height (cm)"
+              outlined
+              required
+              background-color="white"
+          ></v-text-field>
+          <v-text-field
+              v-model="client.weight"
+              :rules="weightRules"
+              label="Weight (kg)"
+              outlined
+              required
+              background-color="white"
+          ></v-text-field>
+          <v-select
+              :items="genders"
+              label="Gender"
+              v-model="client.gender"
+              outlined
+              background-color="white"
+          ></v-select>
+          <v-checkbox
+              color="teal darker-3"
+              v-model="client.wetsuit"
+              :label="`Require wetsuit`"
+              class="pb-7 pt-2 mt-n3"
+          ></v-checkbox>
+        </div>
+      </v-col>
+      <v-col cols="12" md="3">
+        <div class="preview-view">
+          <v-card
+              class="mx-auto"
+              max-width="344"
+              outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-4">
+                  SUMMARY
+                </div>
+                <v-list-item-subtitle>Date: {{ date }}</v-list-item-subtitle>
+                <v-list-item-subtitle>Time: {{ time }}</v-list-item-subtitle>
+                <v-list-item-subtitle>Full name: {{ client.firstName }} {{ client.lastName }}</v-list-item-subtitle>
+                <v-list-item-subtitle>Surf style: {{ client.style.style }}</v-list-item-subtitle>
+                <v-list-item-subtitle>Level: {{ client.level }}</v-list-item-subtitle>
+                <div v-if="client.wetsuit === true">
+                  <v-list-item-subtitle>Require wetsuit: Yes</v-list-item-subtitle>
+                </div>
+                <div v-else>
+                  <v-list-item-subtitle>Require wetsuit: No</v-list-item-subtitle>
+                </div>
+
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+          <div class="booking-btn">
+            <v-btn class=" teal darken-4 white--text" width="100%" max-width="344" outlined text tile x-large
+                   v-on:click="saveClient()">Add person
             </v-btn>
-            <v-btn
-                text
-                color="primary"
-                @click="$refs.menu.save(dates)"
-            >
-              OK
+          </div>
+        </div>
+
+        <div class="preview-view">
+          <v-card
+              class="mx-auto"
+              max-width="344"
+              outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-4">
+                  SUMMARY
+                </div>
+                <table id="clientTable">
+                  <tr v-for="newclient in clients">
+                    <td>{{ newclient.firstName }} {{ newclient.lastName }}</td>
+                    <td>
+                      <v-btn class=" teal darken-4 white--text"
+                             v-on:click="editClient()">Edit
+                      </v-btn>
+                    </td>
+                  </tr>
+                </table>
+                <v-list-item-title class="text-h5 mb-1">
+                  Total: {{ style.price }} $
+                  {{ clients }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+          <div class="booking-btn">
+            <v-btn class=" teal darken-4 white--text" width="100%" max-width="344" outlined text tile x-large
+                   v-on:click="confirmation()">Book
             </v-btn>
-          </v-date-picker>
-        </v-menu>
+          </div>
+        </div>
       </v-col>
     </v-row>
-
-
-    <v-btn v-on:click="confirmation()">Book</v-btn>
   </v-container>
 </template>
 
@@ -130,17 +217,122 @@ import router from "@/router";
 
 export default {
   name: "BookingGroup",
-  data: function () {
-    return {}
-  },
+  data: () => ({
+    date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    menu: false,
+    modal: false,
+    menu2: false,
+    styles: [{style: "Traditional surf", price: 59.99}, {style: "Windsurf", price: 79.99}, {
+      style: "Kitesurf", price: 89.99
+    }],
+    style: {style: "", price: ""},
+    levels: ["Beginner", "Intermediate", "Advanced"],
+    nameRules: [
+      v => !!v || 'Name is required',
+    ],
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      // v => /.+@.+/.test(v) || 'E-mail must be valid',
+    ],
+    weightRules: [
+      v => !!v || 'Weight is required',
+    ],
+    heightRules: [
+      v => !!v || 'Height is required',
+    ],
+    genders: ["Male", "Female"],
+    total: "",
+    times: ["10:00", "11:00", "12:00"],
+    time: "",
+    wetsuit: false,
+    clients: [],
+    client: {
+      style: '',
+      level: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      height: '',
+      gender: '',
+      weight: '',
+      wetsuit: ''
+    },
+    editClientResponse: '',
+  }),
+
   methods: {
     confirmation: function () {
       router.push({name: "BookingConfirmation"});
     }
-  }
+    ,
+    back: function () {
+      router.push({name: "Homepage"})
+    }
+    ,
+    toTop: function () {
+      this.$vuetify.goTo(0)
+    },
+
+    saveClient: function () {
+      let clientCopy = JSON.parse(JSON.stringify(this.client))
+      this.clients.push(clientCopy)
+    },
+
+    editClient: function () {
+      const copyOfClientCopy = clientCopy.map(newclient=>
+      newclient.firstName === firstName ?
+          {...copyOfClientCopy, ...firstName} :newclient)
+
+// objlastName = clients.findIndex((obj => obj.lastName));
+//     })
+},
+},
+mounted()
+{
+  this.toTop()
 }
+}
+
+
 </script>
 
 <style scoped>
+.back-btn {
+  z-index: 2;
+}
 
+.data-input-top {
+  margin: 15% 0 20% 25%;
+}
+
+.data-input-view {
+  margin: 5% 15%;
+}
+
+.preview-view {
+  margin: 15% 15% auto auto;
+}
+
+.booking-btn {
+  text-align: center;
+}
+
+.time-input {
+  display: block;
+}
+
+div >>> .v-card__actions {
+  display: block;
+}
+
+.background-img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 0;
+  opacity: 0.1;
+}
 </style>
